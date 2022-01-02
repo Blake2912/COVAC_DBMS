7a69747cbab0d8c8d29df6e4154e838dd2130ad8
Blake2912 <kcvarun29122001@gmail.com>
2022-01-02 15:56:42 +0530

    Inital Setup and completed with the following pages  - Login  - User Register  - Other registration pages

---
+++ Pages/covac_register.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>COVAC Register</title>
+</head>
+<body>
+    <br>
+    <div class="container">
+        <h2>COVAC User Registration</h2>
+    </div>
+    <div class="container">
+        <h5>Hello user please register here!</h5>
+    </div>
+    <br>
+    <div class="container">
+        <div class="card">
+            <div class="container">
+                <br>
+                <form action="">
+                    <div class="mb-3">
+                        <label for="userIdInput" class="form-label">User ID</label>
+                        <input type="text" class="form-control" id="userIdInput" aria-describedby="UserId" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="firstnameInput" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="firstnameInput" aria-describedby="firstName" placeholder="John" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="lastnameImput" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" id="lastnameInput" aria-describedby="LastName" placeholder="Doe" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="phoneNumberInput" class="form-label">Phone Number</label>
+                        <input type="text" class="form-control" id="phoneNumberInput" aria-describedby="LastName" placeholder="Phone" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="emailInput" class="form-label">Email Address</label>
+                        <input type="email" class="form-control" id="emailInput" aria-describedby="emailInput" placeholder="name@example.com" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="passwordInput" aria-describedby="passwordInput" placeholder="Password" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="aadhaarInput" class="form-label">Aadhaar Number</label>
+                        <input type="text" class="form-control" id="aadhaarInput" aria-describedby="aadhaarInput" placeholder="Aadhaar Number" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="dobInput" class="form-label">Date Of Birth</label>
+                        <input type="date" class="form-control" id="dobInput" aria-describedby="dobInput" required>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </div>
+                    <p class="text-info bg-light">Note: All fields are compulsory</p>
+                </form>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="card">
+            <div class="container">
+                <br>
+                <h6> Hospital/Medical Worker registration click the button below!</h6>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-secondary" id="register">Register Hospital/Worker</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="contaner">
+        <div class="container">
+            <button type="button" class="btn btn-outline-success" id="go_to_login">Go to login</button>
+        </div>
+    </div>
+    <br>
+</body>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+    document.getElementById("register").onclick = function () {
+        location.href = "http://10.211.55.2/COVAC/COVAC_DBMS/Pages/covac_other_reg_type.php";
+    };
+    document.getElementById("go_to_login").onclick = function() {
+        location.href = "http://10.211.55.2/COVAC/COVAC_DBMS/Pages/covac_login.php";
+    };
+</script>
